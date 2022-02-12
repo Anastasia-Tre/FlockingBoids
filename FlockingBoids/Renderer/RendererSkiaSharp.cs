6dc36e79c1eb18185a0c98b96189ff39a849811a
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-02-12 17:28:26 +0200

    add RendererSkiaSharp

---
+++ FlockingBoids/Renderer/RendererSkiaSharp.cs
@@ -0,0 +1,51 @@
+﻿using SkiaSharp;
+
+namespace Renderer
+{
+    public class RendererSkiaSharp : IRenderer
+    {
+        private SKCanvas _canvas;
+        private SKPaint _paint;
+
+        public RendererSkiaSharp(SKCanvas canvas)
+        {
+            _canvas = canvas;
+            _paint = new SKPaint()
+            {
+                IsAntialias = true,
+            };
+        }
+
+        private SKColor ConvertColor(Color color)
+        {
+            return new SKColor(color.R, color.G, color.B, color.A);
+        }
+
+        private SKPoint ConvertPoint(Point pt)
+        {
+            return new SKPoint((float)pt.X, (float)pt.Y);
+        }
+
+        public void Clear(Color color)
+        {
+            _canvas.Clear(ConvertColor(color));
+        }
+
+        public void Dispose()
+        {
+            _paint.Dispose();
+        }
+
+        public void DrawLine(Point pt1, Point pt2, double lineWidth, Color color)
+        {
+            _paint.Color = ConvertColor(color);
+            _canvas.DrawLine(ConvertPoint(pt1), ConvertPoint(pt2), _paint);
+        }
+
+        public void FillCircle(Point center, double radius, Color color)
+        {
+            _paint.Color = ConvertColor(color);
+            _canvas.DrawCircle(ConvertPoint(center), (float)radius, _paint);
+        }
+    }
+}
