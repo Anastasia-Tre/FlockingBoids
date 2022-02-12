13f6801301cd856795ebc16d4735186893fe6eef
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-02-12 16:11:15 +0200

    add class Field

---
+++ FlockingBoids/Model/Field.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Field
+    {
+        float Width, Height;
+        float BoidsNumber;
+
+        public Field(float width, float height)
+        {
+            (Width, Height) = (width, height);
+
+        }
+
+
+    }
+}
