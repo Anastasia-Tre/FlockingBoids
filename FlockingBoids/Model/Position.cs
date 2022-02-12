88d6b56cf5af383597d48f4aa9bd44856d5b0353
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-02-12 16:18:52 +0200

    add Position and Velocity

---
+++ FlockingBoids/Model/Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Position
+    {
+        public float X, Y;
+
+        public Position(float x, float y)
+        {
+            (X, Y) = (x, y);
+        }
+
+        public void Move(Velocity velocity, float step)
+        {
+            X += velocity.X * step;
+            Y += velocity.Y * step;
+        }
+
+
+    }
+}
---
+++ FlockingBoids/Model/Velocity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Velocity
+    {
+        public float X, Y;
+
+        public Velocity(float x, float y)
+        {
+            (X, Y) = (x, y);
+        }
+    }
+}
