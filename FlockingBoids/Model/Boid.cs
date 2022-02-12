dd78cbd9ce53f450da0e8fa4a9f7d4e41cd35e00
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-02-12 16:14:08 +0200

    add class Boid

---
+++ FlockingBoids/Model/Boid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Boid
+    {
+        public float X, Y;
+        public float Velocity;
+
+        public Boid(float x, float y)
+        {
+            (X, Y) = (x, y);
+        }
+    }
+}
