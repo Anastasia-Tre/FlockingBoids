546f9963ca7275ed84c6f029579cccbe6ec04d42
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-02-14 16:11:47 +0200

    add class Behaviour

---
+++ FlockingBoids/Model/Behaviour/Behaviour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Behaviour
+{
+    public abstract class Behaviour
+    {
+        public float Weight;
+        public float Distance;
+        public Boid[] Boids;
+
+        public Behaviour(Boid[] boids, float distance, float weight)
+        {
+            Boids = boids;
+            Distance = distance;
+            Weight = weight;
+        }
+
+        public abstract Velocity GetVelocity(Boid currentBoid);
+    }
+}
