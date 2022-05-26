012066a9a80153ad54f135ab08e0c6f34050e1b2
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-05-26 21:21:37 +0200

    add method SetSpeed

--- FlockingBoids/Model/Velocity.cs
+++ FlockingBoids/Model/Velocity.cs
@@ -11,14 +11,28 @@ namespace Model
             (X, Y) = (x, y);
         }
 
-        public double GetAngle()
+        public float GetAngle()
         {
             if (X == 0 && Y == 0)
                 return 0;
-            var angle = Math.Atan(Y / X) * 180 / Math.PI - 90;
+            var angle = (float)(Math.Atan(Y / X) * 180 / Math.PI - 90);
             if (X < 0)
                 angle += 180;
             return angle;
+        }
+
+        public void SetSpeed(float speed)
+        {
+            if (X == 0 && Y == 0)
+                return;
+
+            var currentSpeed = (float)Math.Sqrt(X * X + Y * Y);
+
+            var targetX = X / currentSpeed * speed;
+            var targetY = Y / currentSpeed * speed;
+
+            X = targetX;
+            Y = targetY;
         }
     }
 }
