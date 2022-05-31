adcd86b64b4a8f5bb904e5a70e47fee354dc513e
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-05-31 19:44:39 +0200

    add to View method Reset

--- FlockingBoids/View/MainWindow.xaml.cs
+++ FlockingBoids/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Model;
 using Renderer;
 
 namespace View
@@ -8,14 +9,29 @@ namespace View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Field field;
+
         public MainWindow()
         {
             InitializeComponent();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            field = new Field(
+                width: (float)ResultField.Width,
+                height: (float)ResultField.Height,
+                boidsCount: 100, // to fix
+                enemyCount: 10 // to fix
+            );
+
         }
 
         private void SKElement_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
         {
             var renderer = new RendererSkiaSharp(e.Surface.Canvas);
+            renderer.Render(field);
         }
     }
 }
