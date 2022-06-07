e5e38a35bc9c16495d6960ec621f4079fd1c8227
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-07 14:35:27 +0200

    add list of behaviours to Boid

--- FlockingBoids/Model/Boid.cs
+++ FlockingBoids/Model/Boid.cs
@@ -1,4 +1,6 @@
-﻿namespace Model
+﻿using System.Collections.Generic;
+
+namespace Model
 {
     public class Boid
     {
@@ -6,12 +8,19 @@
         public Position Position;
         public float Speed;
         public Velocity Velocity;
-        public Behaviour[] Behaviours;
+        private readonly List<Behaviour> behaviours;
 
         public Boid(float x, float y, float xVel, float yVel)
         {
             Position = new Position(x, y);
             Velocity = new Velocity(xVel, yVel);
+            behaviours = new List<Behaviour>();
         }
+
+        public void AddBehaviour(Behaviour behaviour)
+        {
+            behaviours.Add(behaviour);
+        }
+        
     }
 }
