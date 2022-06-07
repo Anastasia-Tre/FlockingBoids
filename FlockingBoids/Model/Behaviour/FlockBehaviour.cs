d9841228112fd6c925157aad052c95a4ad04e85d
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-07 09:50:14 +0200

    add class FlockBehaviour

---
+++ FlockingBoids/Model/Behaviour/FlockBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Behaviour
+{
+    internal class FlockBehaviour : Behaviour
+    {
+        public FlockBehaviour(Boid[] boids, float distance, float weight) : 
+            base(boids, distance, weight)
+        {
+        }
+
+        public override Velocity GetVelocity(Boid currentBoid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
