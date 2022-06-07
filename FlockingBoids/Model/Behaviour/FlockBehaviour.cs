48b4d9d0e8659ea9216afff3cfbbb414d993c3d3
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-07 14:31:29 +0200

    add implementation of FlockBehaviour

--- FlockingBoids/Model/Behaviour/FlockBehaviour.cs
+++ FlockingBoids/Model/Behaviour/FlockBehaviour.cs
@@ -15,7 +15,30 @@ namespace Model
 
         public override Velocity GetVelocity(Boid currentBoid)
         {
-            
+            if (currentBoid.IsEnemy)
+                Distance *= 2;
+
+            int neighborCount = 0;
+            float centerX = 0;
+            float centerY = 0;
+            foreach (Boid boid in Boids)
+            {
+                if (boid.Position.Distance(currentBoid.Position) < Distance)
+                {
+                    centerX += boid.Position.X;
+                    centerY += boid.Position.Y;
+                    neighborCount += 1;
+                }
+            }
+            centerX /= neighborCount;
+            centerY /= neighborCount;
+
+            // steer toward the flock
+            var X = (centerX - currentBoid.Position.X) * Weight;
+            var Y = (centerY - currentBoid.Position.Y) * Weight;
+
+            var resultVelocity = new Velocity(X, Y);
+            return resultVelocity;
         }
     }
 }
