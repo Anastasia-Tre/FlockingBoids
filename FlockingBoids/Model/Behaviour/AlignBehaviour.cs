4f812a1325c261925cfc22b01f31cad599ef60ef
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-09 15:00:29 +0200

    implement GetVelocity for AlignBehaviour

--- FlockingBoids/Model/Behaviour/AlignBehaviour.cs
+++ FlockingBoids/Model/Behaviour/AlignBehaviour.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Model.Behaviour
 {
-    internal class AlignBehaviour
+    internal class AlignBehaviour : Behaviour
     {
+        public AlignBehaviour(Boid[] boids, float distance, float weight) : base(boids, distance, weight)
+        {
+        }
+
+        public override Velocity GetVelocity(Boid currentBoid)
+        {
+            // determine mean velocity of the flock
+            int neighborCount = 0;
+            Velocity meanVelocity = new Velocity(0, 0);
+            foreach (Boid boid in Boids)
+            {
+                if (boid.Position.Distance(currentBoid.Position) < Distance)
+                {
+                    meanVelocity += boid.Velocity;
+                    neighborCount += 1;
+                }
+            }
+            meanVelocity /= neighborCount;
+
+            //Vel.X -= (Vel.X - meanVelX) * weight;
+            //Vel.Y -= (Vel.Y - meanVelY) * weight;
+
+            return meanVelocity;
+        }
     }
 }
