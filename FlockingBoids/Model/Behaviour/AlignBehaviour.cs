9fba3181653f71ca78559a3032df7c6567ca89ba
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-09 15:02:59 +0200

    fix style

--- FlockingBoids/Model/Behaviour/AlignBehaviour.cs
+++ FlockingBoids/Model/Behaviour/AlignBehaviour.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Model.Behaviour
+﻿namespace Model.Behaviour
 {
     internal class AlignBehaviour : Behaviour
     {
