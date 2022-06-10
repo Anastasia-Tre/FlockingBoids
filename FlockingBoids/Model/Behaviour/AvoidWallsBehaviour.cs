b76eea3ae41a74425c39e4e1dc4386737dd0140e
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-10 15:49:46 +0200

    implement Behaviour class

--- FlockingBoids/Model/Behaviour/AvoidWallsBehaviour.cs
+++ FlockingBoids/Model/Behaviour/AvoidWallsBehaviour.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Model.Behaviour
 {
-    internal class AvoidWallsBehaviour
+    internal class AvoidWallsBehaviour : Behaviour
     {
+        public AvoidWallsBehaviour(Boid[] boids, float distance, float weight) : base(boids, distance, weight)
+        {
+        }
+
+        public override Velocity GetVelocity(Boid currentBoid)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
