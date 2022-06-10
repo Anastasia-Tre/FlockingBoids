6338eea201e20ec0921bc360b302fbb69acf2c58
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-10 15:58:44 +0200

    implement GetVelocity in AvoidWallsBehaviour

--- FlockingBoids/Model/Behaviour/AvoidWallsBehaviour.cs
+++ FlockingBoids/Model/Behaviour/AvoidWallsBehaviour.cs
@@ -7,14 +7,26 @@ namespace Model.Behaviour
         private const float Pad = 100;
         private const float Turn = 0.1f;
 
+        private readonly float _width;
+        private readonly float _height;
+
         public AvoidWallsBehaviour(Boid[] boids, float width, float height, float weight) 
             : base(boids, Pad, weight)
         {
+            _width = width;
+            _height = height;
         }
 
         public override Velocity GetVelocity(Boid currentBoid)
         {
-            throw new NotImplementedException();
+            var resultVelocity = new Velocity(0, 0);
+
+            if (currentBoid.Position.X < Pad) resultVelocity.X += Turn;
+            if (currentBoid.Position.Y < Pad) resultVelocity.Y += Turn;
+            if (currentBoid.Position.X > _width - Pad) resultVelocity.X -= Turn;
+            if (currentBoid.Position.Y > _height - Pad) resultVelocity.Y -= Turn;
+
+            return resultVelocity;
         }
     }
 }
