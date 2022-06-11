4cd06e7ed1173ead64b7042231550174a5e6e17b
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-11 12:32:42 +0200

    fix CalcVelocity in AvoidBoidsBehaviour

--- FlockingBoids/Model/Behaviour/AvoidBoidsBehaviour.cs
+++ FlockingBoids/Model/Behaviour/AvoidBoidsBehaviour.cs
@@ -8,59 +8,8 @@
 
         public override void CalcVelocity(Boid currentBoid)
         {
-            var resultVelocity = new Velocity(0, 0);
-            for (int i = 0; i < Boids.Length; i++) // rewrite to foreach
-            {
-                Boid boid = Boids[i];
-                /*if (boid.IsEnemy && boid.Position.Distance(currentBoid.Position) < Distance)
-                {
-                    if (boid.Position.Distance(currentBoid.Position) < Distance)
-                    {
-                        //Vel.X -= (boid.Pos.X - Pos.X) * weight;
-                        //Vel.Y -= (boid.Pos.Y - Pos.Y) * weight;
-
-                        //currentBoid.Velocity -= (boid.Position - currentBoid.Position) * Weight;
-                        resultVelocity = (boid.Position - currentBoid.Position);
-                    }
-                }*/
-                /*float closeness = 1;
-                if (boid.IsEnemy != currentBoid.IsEnemy)
-                {
-                    closeness *= 2;
-
-                }
-                closeness *= Distance - boid.Position.Distance(currentBoid.Position);
-                if (closeness > 1)
-                {
-                    // avoid with a magnitude correlated to closeness
-                    //Vel.X -= (boid.Pos.X - Pos.X) * weight * closeness;
-                    //Vel.Y -= (boid.Pos.Y - Pos.Y) * weight * closeness;
-                    resultVelocity = (boid.Position - currentBoid.Position);
-                }*/
-
-                /*if (boid.Position.Distance(currentBoid.Position) < Distance)
-                {
-                    if (boid.IsEnemy)
-                    {
-                        currentBoid.Velocity -= (boid.Position - currentBoid.Position) * 2 * Weight;
-                    }
-                    currentBoid.Velocity -= (boid.Position - currentBoid.Position) * Weight;
-                }*/
-
-
-
-
-            }
-
             foreach (var boid in Boids)
             {
-                if (boid.IsEnemy && boid.Position.Distance(currentBoid.Position) < Distance)
-                {
-                    if (boid.Position.Distance(currentBoid.Position) < Distance)
-                    {
-                        currentBoid.Velocity -= (boid.Position - currentBoid.Position) * Weight;
-                    }
-                }
                 if (boid.IsEnemy == currentBoid.IsEnemy) 
                 {
                     float closeness = Distance - boid.Position.Distance(currentBoid.Position);
@@ -70,9 +19,6 @@
                     }
                 }
             }
-
-
-            //currentBoid.Velocity -= resultVelocity * Weight;
         }
     }
 }
