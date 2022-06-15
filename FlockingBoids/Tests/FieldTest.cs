3aec28075a1ba5e92fd83d9d21b24be03aa34166
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-15 11:05:59 +0200

    fix style

--- FlockingBoids/Tests/FieldTest.cs
+++ FlockingBoids/Tests/FieldTest.cs
@@ -1,34 +1,35 @@
 using Model;
 
-namespace Tests
+namespace Tests;
+
+public class FieldTest
 {
-    public class FieldTest
+    private const int Size = 100;
+    private Field? _field;
+
+    [Fact]
+    public void TestExceptionBoidNumber()
     {
-        private const int Size = 100;
-
-        [Fact]
-        public void TestExceptionBoidNumber()
+        Assert.Throws<Exception>(() =>
         {
-            Assert.Throws<Exception>(() =>
-            {
-                var field = new Field(Size, Size, Size, Size + 1);
-            });
-        }
+            _field = new Field(Size, Size, Size, Size + 1);
+        });
+    }
 
-        [Fact]
-        public void TestBoidNumber()
-        {
-            var field = new Field(Size, Size, Size, Size);
-            var counter = field.Boids.Count(boid => boid.IsEnemy);
-            Assert.Equal(counter, Size);
-        }
+    [Fact]
+    public void TestBoidNumber()
+    {
+        _field = new Field(Size, Size, Size, Size);
+        var counter = _field.Boids.Count(boid => boid.IsEnemy);
+        Assert.Equal(counter, Size);
+    }
 
-        [Fact]
-        public void TestExceptionFieldSize()
+    [Fact]
+    public void TestExceptionFieldSize()
+    {
+        Assert.Throws<Exception>(() =>
         {
-            Assert.Throws<Exception>(() => {
-                var field = new Field(0, 0, Size, Size);
-            });
-        }
+            _field = new Field(0, 0, Size, Size);
+        });
     }
 }
