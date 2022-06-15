7b71909bb6c4a6058d3521b41ddd68bab80579e9
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-06-15 19:13:31 +0200

    add consts to Controller

--- FlockingBoids/Controller/Controller.cs
+++ FlockingBoids/Controller/Controller.cs
@@ -8,11 +8,14 @@ namespace Controller
         public Field Field { get; private set; }
         public IRenderer Renderer { get; private set; }
 
+        private const int BoidsCount = 500;
+        private const int EnemyCount = 5;
+
         public void CreateField()
         {
             Field = new Field(
-                500, // to fix
-                5 // to fix
+                BoidsCount,
+                EnemyCount
             );
         }
 
